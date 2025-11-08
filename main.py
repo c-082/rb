776d8107bbe5d3106cd43049012d93b8b2132010
theoriginalralsei <sibolinaofrancisinigo@Gmail.com>
2025-11-08 10:09:50 +0800

    First Commit

---
+++ main.py
@@ -0,0 +1,95 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+import asyncio
+import random
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+bot = commands.Bot(command_prefix="r:", intents=intents)
+
+@bot.event
+async def on_ready():
+    try:
+        sync = await bot.tree.sync()
+        print(f"Synced {len(sync)} commands!")
+        print(f"Logged in as {bot.user}!")
+    except Exception as e:
+        print(f"Failed to load commands: {e}")
+
+class Greeting(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self,member):
+        channel = discord.utils.get(member.guild.text_channels, name="✦-welcomes")
+        if channel:
+            await channel.send(f"Welcome {member.mention} to Ralsei's Castle Town!")
+
+
+@bot.event
+async def on_command_error(ctx,error):
+    await ctx.send(f"Error: {error}")
+
+@bot.tree.command(name="commands", description="Shows all available commands")
+async def show_commands(interaction: discord.Interaction):
+    embed = discord.Embed(
+            title="Commands",
+            description=None,
+            color=discord.Color.green()
+            )
+
+    slash_commands = []
+    for cmd in bot.tree.get_commands():
+        slash_commands.append(f"/{cmd.name} - {cmd.description}")
+    
+    if slash_commands:
+
+        embed.add_field(
+            name="Slash Commands",
+            value="\n".join(slash_commands),
+            inline=False
+        )
+
+    for cog_name, cog in bot.cogs.items():
+        commands_list = cog.get_commands()
+
+        if commands_list:
+
+            commands_info = "\n".join([f"r:{cmd.name}" for cmd in commands_list])
+            embed.add_field(
+                name=f"{cog_name} Commands",
+                value=commands_info,
+                inline=False
+            ) 
+
+
+    await interaction.response.send_message(embed=embed)  
+
+@bot.event
+async def on_message_sent(message):
+    counter = 0
+    if message.author.id == 1367051487968100443 and counter < 1:
+        await ctx.send("We miss you..")
+        counter += 1
+
+
+async def main():
+    await bot.add_cog(Greeting(bot))
+    extensions = ["cogs.fun", "cogs.admin", "cogs.ac"]
+
+    for ex in extensions:
+        try:
+            await bot.load_extension(ex)
+            print(f"Loaded {ex}!")
+        except Exception as e:
+            print(f"Failed to load {ex}, reason {e}")
+    await bot.start(TOKEN)
+
+asyncio.run(main())
