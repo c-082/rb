31ababab45f7cb226d1e8120039ba2eb0425f269
Ralsei <sibolinaofrancisinigo@gmail.com>
2025-12-15 15:09:17 +0800

    Fixed the slap

--- cogs/actions.py
+++ cogs/actions.py
@@ -3,77 +3,69 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
+
 class Action(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
 
     @app_commands.command(name="hug", description="Hugs someone :3")
     async def hug(self, interaction: discord.Interaction, member: discord.Member):
         if member.id == 1434674677682409573:
             messages = [
-                    "M...M-me?!",
-                    "Thanks...i needed it.",
-                    "Can...can we marry now?"
-                    ]
+                "M...M-me?!",
+                "Thanks...i needed it.",
+                "Can...can we marry now?",
+            ]
         else:
-            messages = [
-                    "Awww!",
-                    "So Adorable!!!",
-                    "Nice...Marriage when-"
-                    ]
+            messages = ["Awww!", "So Adorable!!!", "Nice...Marriage when-"]
 
         links = [
-                "https://media1.tenor.com/images/xzhJUwNq1jkAAAAC/norep-owen.gif",
-                "https://media1.tenor.com/images/iDU4OvXi1dUAAAAC/ralsei-kris.gif",
-                "https://media1.tenor.com/images/MUJNqPh0MhwAAAAC/pixel-art-hug.gif",
-                "https://media.tenor.com/j2qBShhqdUYAAAAi/hugging-hug.gif" 
-                ] 
+            "https://media1.tenor.com/images/xzhJUwNq1jkAAAAC/norep-owen.gif",
+            "https://media1.tenor.com/images/iDU4OvXi1dUAAAAC/ralsei-kris.gif",
+            "https://media1.tenor.com/images/MUJNqPh0MhwAAAAC/pixel-art-hug.gif",
+            "https://media.tenor.com/j2qBShhqdUYAAAAi/hugging-hug.gif",
+        ]
 
         chosen_gif = random.choice(links)
         chosen_message = random.choice(messages)
 
         embed = discord.Embed(
-               title=f"{interaction.user.display_name} has hugged {member.display_name}!",
-               description=chosen_message,
-               color=discord.Color.green()
-               )
-        
+            title=f"{interaction.user.display_name} has hugged {member.display_name}!",
+            description=chosen_message,
+            color=discord.Color.green(),
+        )
+
         embed.set_image(url=chosen_gif)
 
         await interaction.response.send_message(embed=embed)
-  
+
     @app_commands.command(name="kiss", description="Kisses someone :3")
     async def kiss(self, interaction: discord.Interaction, member: discord.Member):
         if member.id == 1434674677682409573:
             messages = [
-                    "M...M-me?!",
-                    "Thanks...i needed it.",
-                    "Can...can we marry now?",
-                    "Ummm...UwU!"
-                    ]
+                "M...M-me?!",
+                "Thanks...i needed it.",
+                "Can...can we marry now?",
+                "Ummm...UwU!",
+            ]
         else:
-            messages = [
-                    "Awww!",
-                    "So Adorable!!!",
-                    "Nice...Marriage when-"
-                    ]
+            messages = ["Awww!", "So Adorable!!!", "Nice...Marriage when-"]
 
         links = [
-                "https://media1.tenor.com/images/qIngZUPdp0gAAAAC/kiss-gif-kiss-gif-couple.gif",
-                "https://media1.tenor.com/images/iNfNEvkkyT0AAAAC/deltarune-ralsei.gif"
-                "https://media.tenor.com/3dTj9cRPU50AAAAj/ralsei-kris.gif"
-                ] 
+            "https://media1.tenor.com/images/qIngZUPdp0gAAAAC/kiss-gif-kiss-gif-couple.gif",
+            "https://media1.tenor.com/images/iNfNEvkkyT0AAAAC/deltarune-ralsei.gif"
+            "https://media.tenor.com/3dTj9cRPU50AAAAj/ralsei-kris.gif",
+        ]
 
         chosen_gif = random.choice(links)
         chosen_message = random.choice(messages)
 
         embed = discord.Embed(
-               title=f"{interaction.user.display_name} has kissed {member.display_name}",
-               description=chosen_message,
-               color=discord.Color.green()
-               )
-         
+            title=f"{interaction.user.display_name} has kissed {member.display_name}",
+            description=chosen_message,
+            color=discord.Color.green(),
+        )
+
         embed.set_image(url=chosen_gif)
 
         await interaction.response.send_message(embed=embed)
@@ -82,61 +74,60 @@ class Action(commands.Cog):
     async def headpat(self, interaction: discord.Interaction, member: discord.Member):
         if member.id == 1434674677682409573:
             messages = [
-                    "T-too many headpats!!!! >_<",
-                    "Im...Im not a dog, Human!",
-                    "UwU- AH I DIDN'T MEAN TO SAY THAT IM SO SORRY- I- UHH- I- I- uwu~"
-                    ]
+                "T-too many headpats!!!! >_<",
+                "Im...Im not a dog, Human!",
+                "UwU- AH I DIDN'T MEAN TO SAY THAT IM SO SORRY- I- UHH- I- I- uwu~",
+            ]
 
-        else: 
+        else:
             messages = [
-                    "Pat pat pat pat! ^_^",
-                    "It's like they're petting a dog! >_O",
-                    "Awwwwwww!!!!"
-                    ]
-
+                "Pat pat pat pat! ^_^",
+                "It's like they're petting a dog! >_O",
+                "Awwwwwww!!!!",
+            ]
 
         links = [
-               "https://media.tenor.com/images/Lx6pxlvj44gAAAAC/ralsei-deltarune.gif",
-               "https://media.tenor.com/images/CnXZJo9ddGIAAAAC/ralsei-cute-ralsei.gif",
-               "https://media.tenor.com/lHCzlH_ez84AAAAi/cute-love.gif",
-               "https://media.tenor.com/xFEinJ1BTosAAAAi/cartoon-patting.gif"
-               ] 
+            "https://media.tenor.com/images/Lx6pxlvj44gAAAAC/ralsei-deltarune.gif",
+            "https://media.tenor.com/images/CnXZJo9ddGIAAAAC/ralsei-cute-ralsei.gif",
+            "https://media.tenor.com/lHCzlH_ez84AAAAi/cute-love.gif",
+            "https://media.tenor.com/xFEinJ1BTosAAAAi/cartoon-patting.gif",
+        ]
 
         chosen_gif = random.choice(links)
         chosen_message = random.choice(messages)
 
         embed = discord.Embed(
-               title=f"{interaction.user.display_name} has headpatted {member.display_name}",
-               description=chosen_message,
-               color=discord.Color.green()
-               )
-        
+            title=f"{interaction.user.display_name} has headpatted {member.display_name}",
+            description=chosen_message,
+            color=discord.Color.green(),
+        )
+
         embed.set_image(url=chosen_gif)
 
         await interaction.response.send_message(embed=embed)
 
-    @app_commands.command(name="slap", description="Slap anyone wwho gets in ya way >:D")
+    @app_commands.command(
+        name="slap", description="Slap anyone wwho gets in ya way >:D"
+    )
     async def slap(self, interaction: discord.Interaction, member: discord.Member):
         if member.id == 1434674677682409573:
-            messages = [
-                    "O-ow! >_<",
-                    "That was painful!",
-                    "Stop!!!"
-                    ]
+            messages = ["O-ow! >_<", "That was painful!", "Stop!!!"]
         else:
             messages = [
-                    "damn..that looked painful",
-                    "oof, goddamn",
-                    "Whoa..wouldn't wanna be that guy, am i right?"
-                    ]
+                "damn..that looked painful",
+                "oof, goddamn",
+                "Whoa..wouldn't wanna be that guy, am i right?",
+            ]
 
         chosen_message = random.choice(messages)
 
         emdbed = discord.Embed(
-                title=f"{interaction.user.display_name} has slapped {member.display_name}",
-                description=chosen_message,
-                color=discord.Color.green()
-                )
+            title=f"{interaction.user.display_name} has slapped {member.display_name}",
+            description=chosen_message,
+            color=discord.Color.green(),
+        )
+
+        await interaction.response.send_message(embed=emdbed)
 
 
 async def setup(bot):
