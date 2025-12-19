34be7cbc9437075119116c8a2c14f33daf97b50d
Ralsei <sibolinaofrancisinigo@gmail.com>
2025-12-19 15:16:45 +0800

    Added some groupings, nothing much

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,8 +1,7 @@
-from enum import verify
 import discord
 from discord.ext import commands
 import random
-from discord import app_commands, embeds
+from discord import app_commands, client, embeds
 
 arr_truth = [
     "When was ya' first kiss?",
@@ -56,20 +55,40 @@ class TODView(discord.ui.View):
 
 
 class Fun(commands.Cog):
+    fun_slash_group = app_commands.Group(
+        name="fun", description="For all your funni needs :3"
+    )
+
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.group(name="fun", invoke_without_command=True)
+    async def fun(self, ctx):
+        if ctx.invoke_subcommand is None:
+            embed = discord.Embed(
+                title="Fun commands", description="", color=discord.Color.green()
+            )
+
+            for command in self.fun.commands:
+                embed.add_field(
+                    name="",
+                    value=f"{command.name} - {command.help or 'No description provided'}",
+                    inline=True,
+                )
+
+            await ctx.send(embed=embed)
 
     def coinflip(self):
         return random.randint(1, 2)
 
-    @commands.command(name="coinflip", aliases=["cf"])
+    @fun.command(name="coinflip", aliases=["cf"])
     async def coinflip_message(self, ctx):
         if self.coinflip() == 1:
             await ctx.channel.send("1. Heads")
         else:
             await ctx.channel.send("2. Tails")
 
-    @commands.command(name="Scream", aliases=["scream", "s"])
+    @fun.command(name="Scream", aliases=["scream", "s"])
     async def Scream(self, ctx):
         embed = discord.Embed(
             title=None,
@@ -79,7 +98,7 @@ class Fun(commands.Cog):
 
         await ctx.send(embed=embed)
 
-    @commands.command(name="Speak", aliases=["speak", "sp"])
+    @fun.command(name="Speak", aliases=["speak", "sp"])
     async def Speak(self, ctx, *msg):
         embed = discord.Embed(
             title=None,
@@ -89,7 +108,7 @@ class Fun(commands.Cog):
 
         await ctx.send(embed=embed)
 
-    @app_commands.command(
+    @fun_slash_group.command(
         name="8ball", description="Talk with Ralsei of True Wisdom and Knowledge"
     )
     async def ball(self, interaction: discord.Interaction, message: str):
@@ -112,13 +131,17 @@ class Fun(commands.Cog):
             "https://www.demirramon.com/gen/undertale_text_box.png?text=Don%27t&box=deltarune&character=deltarune-ralsei&expression=shadow&size=2&t=1764839390",
             "https://www.demirramon.com/gen/undertale_text_box.png?text=It%27s%20in%20Prophecy%20little%20bro.&box=deltarune&character=deltarune-ralsei&expression=winking&size=2&t=1764839486",
             "https://www.demirramon.com/gen/undertale_text_box.png?text=UwU%0Acolor%3D%23808080%20(%20it%27s%20ralsei%27s%20way%20of%20saying%20yes%20)%20color%3Dwhite&box=deltarune&character=deltarune-ralsei&expression=excited-grin&size=2&t=1764839634",
+            "https://www.demirramon.com/gen/undertale_text_box.png?text=You%20shouldn%27t.%20&box=deltarune&character=deltarune-ralsei&expression=shadow&size=1&t=1765851328"
+            "https://www.demirramon.com/gen/undertale_text_box.png?text=I%20mean%20ig%20idrk%20&box=deltarune&character=deltarune-ralsei&expression=uninterested&size=1&t=1765851450"
+            "https://www.demirramon.com/gen/undertale_text_box.png?text=WHAT%20KINDA%20QUESTION%20IS%20THAT%3F%3F%3F%3F%3F%3F%3F&box=deltarune&character=deltarune-ralsei&expression=screaming&size=1&t=1765851556"
+            "https://www.demirramon.com/gen/undertale_text_box.png?text=Fuck%20you%20mean%2C%20mate%3F&box=deltarune&character=deltarune-ralsei&expression=surprised-looking-away&size=1&t=1765851501",
         ]
 
         embed.set_image(url=random.choice(responses))
 
         await interaction.response.send_message(embed=embed)
 
-    @app_commands.command(
+    @fun_slash_group.command(
         name="truth_or_dare", description="Play some TOD with Ralsei!"
     )
     async def tod(self, interaction: discord.Interaction):
