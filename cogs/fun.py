7878db0a5b05e13a285d2e64480551b9a93e8107
theoriginalralsei <sibolinaofrancisinigo@gmail.com>
2026-03-04 13:48:11 +0800

    refactor: add TOD command to fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -3,6 +3,52 @@ from discord.ext import commands
 import random
 from discord import app_commands
 
+arr_truth = [
+    "When was ya' first kiss?",
+    "What's the most Femboy thing you've ever done?",
+    "If you had a first date (if so then why are you here-), what was it like?",
+    "Weirdest moment?",
+    "How much of a nerd are you on a scale of 1-10?",
+    "Have you ever had a crush on someone here?",
+]
+
+arr_dare = [
+    "DM someone here a kiss and say nothing",
+    "Show off your thighs to one person",
+    "Show off your thighs to EVERYONE",
+    "Share a recent shower thought you had",
+    "Recreate this bot, now."
+]
+
+class TODView(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @discord.ui.button(label="Truth", style=discord.ButtonStyle.green)
+    async def truth_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        question = random.choice(arr_truth)
+        embed = discord.Embed(
+            title="Truth", description=f"**{question}**", color=discord.Color.green()
+        )
+        await interaction.response.send_message(embed=embed, view=self)
+
+    @discord.ui.button(label="Dare", style=discord.ButtonStyle.red)
+    async def dare_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        dare = random.choice(arr_dare)
+        embed = discord.Embed(
+            title="Dare", description=f"**{dare}**", color=discord.Color.red()
+        )
+        await interaction.response.send_message(embed=embed, view=self)
+
+    @discord.ui.button(label="Random", style=discord.ButtonStyle.secondary)
+    async def random_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        random_tod = random.choice([random.choice(arr_truth), random.choice(arr_dare)])
+        embed = discord.Embed(
+            title="Random", description=f"**{random_tod}**", color=0xFFFFFE
+        )
+
+        await interaction.response.send_message(embed=embed, view=self)
+
 class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -59,6 +105,16 @@ class Fun(commands.Cog):
         embed.set_image(url=random.choice(responses))
 
         await interaction.response.send_message(embed=embed)
+
+    @app_commands.command(name="tod", description="Play some TOD with Ralsei bot!")
+    async def tod(self, interaction: discord.Interaction):
+        view = TODView()
+        embed = discord.Embed(
+                title="Truth Or Dare",
+                description="-# why did i make this",
+                color=discord.Color.blue(),
+                )
+        await interaction.response.send_message(embed=embed, view=view)
 
 
 async def setup(bot):
